f98838521f3cab30c1391d07d9e327473ff15875
jinghewang <jinghewang@163.com>
2018-03-06 12:00:44 +0800

    add library

---
+++ CommonLibrary/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CommonLibrary
+{
+    public class Test
+    {
+
+        public string Hello(string name)
+        {
+            return "hello:" + name;
+        }
+        
+    }
+}
