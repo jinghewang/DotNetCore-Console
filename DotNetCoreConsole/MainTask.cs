4defe745c88cd4d92c38446c8c96ef492f7414c3
jinghewang <jinghewang@163.com>
2018-03-06 12:58:25 +0800

    add maintast

---
+++ DotNetCoreConsole/MainTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using CommonLibrary;
+using CommonLibrary.Helpers;
+using DotNetCoreConsole.Utils;
+
+namespace DotNetCoreConsole
+{
+    public class MainTask
+    {
+        public void run(string[] args)
+        {
+            //test
+            Test test = new Test();
+            Console.WriteLine(test.Hello("wjh"));
+            
+            //utils
+            UtilsHelper test2 = new UtilsHelper();
+            Console.WriteLine(test2.Hello("wjh"));
+            
+            //path
+            string path = Directory.GetCurrentDirectory();
+            Console.WriteLine(path);
+            
+            string separatorChar = FileUtils.getDirectorySeparatorChar(path);
+            string newPath = path + separatorChar + "123";
+            Console.WriteLine(newPath);
+
+            return ;
+            
+            DirectoryInfo dinfo = Directory.CreateDirectory(newPath);
+            for(int i=0;i<10;i++){
+                Console.WriteLine("Hello World!");
+            }
+        }
+        
+        private static string getName(int i)
+        {
+            return "wjh" + i.ToString();
+        }
+    }
+}
--- DotNetCoreConsole/Program.cs
+++ DotNetCoreConsole/Program.cs
@@ -11,35 +11,8 @@ namespace DotNetCoreConsole
     {
         static void Main(string[] args)
         {
-            //test
-            Test test = new Test();
-            Console.WriteLine(test.Hello("wjh"));
-            
-            //utils
-            UtilsHelper test2 = new UtilsHelper();
-            Console.WriteLine(test2.Hello("wjh"));
-            
-            //path
-            string path = Directory.GetCurrentDirectory();
-            Console.WriteLine(path);
-            
-            string separatorChar = FileUtils.getDirectorySeparatorChar(path);
-            string newPath = path + separatorChar + "123";
-            Console.WriteLine(newPath);
-
-            return ;
-            
-            DirectoryInfo dinfo = Directory.CreateDirectory(newPath);
-            for(int i=0;i<10;i++){
-                Console.WriteLine("Hello World!");
-            }
+            MainTask mainTask = new MainTask();
+            mainTask.run(args);
         }
-        
-        
-        private static string getName(int i)
-        {
-            return "wjh" + i.ToString();
-        }
-        
     }
 }
